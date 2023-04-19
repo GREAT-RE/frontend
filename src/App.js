da264e3e73dde072790644a07d705b58126c3190
PedroGLL27 <pedrolou27@gmail.com>
2023-04-19 23:06:48 +0100

    homePage--> HomePage WIP

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage';
 import './App.css';
 
 function App() {
   return (
     <Router>
-      <Route exact path="/" render={() => <HomePage />} />
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+      </Routes>
     </Router>
   );
 }
