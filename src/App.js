8eaf1904f710f01844963ab8313bed2f7e99e394
PedroGLL27 <pedrolou27@gmail.com>
2023-04-19 23:06:21 +0100

    homePage--> HomePage WIP

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage/HomePage';
 import './App.css';
 
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <Router>
+      <Route exact path="/" render={() => <HomePage />} />
+    </Router>
   );
 }
 
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,2 +1,13 @@
-import React from "react";
-import "./HomePage.css";
+import React from 'react';
+import './HomePage.css';
+import homeBackground from "../../assets/homeBackgroung.png"
+
+function HomePage() {
+  return (
+    <div className="page">
+      <div className="home-content" style={{ backgroundImage: `url(${homeBackground})`, backgroundSize: "cover" }}></div>
+    </div>
+  );
+}
+
+export default HomePage;
