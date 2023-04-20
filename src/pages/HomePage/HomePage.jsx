3b67b39a2b19112c0df7eb4ed763bc759b8c0bc5
PedroGLL27 <pedrolou27@gmail.com>
2023-04-20 11:25:52 +0100

    homePage--> Home page WIP

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import './HomePage.css';
-import homeBackground from "../../assets/homeBackgroung.png"
+import homeBackground from "../../assets/homeBackground.jpg";
+import logo from "../../assets/logoHILX.png";
 
 function HomePage() {
   return (
     <div className="page">
-      <div className="home-content" style={{ backgroundImage: `url(${homeBackground})`, backgroundSize: "cover" }}></div>
+      <div className="home-content">
+        <div className="image-container" style={{ backgroundImage: `url(${homeBackground})` }}>
+          <div className="logo-container">
+            <img src={logo} alt="Logo" className="logo" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
