1c9a7f5eb222a08020534243cdf2719d9f5043b9
PedroGLL27 <pedrolou27@gmail.com>
2023-04-20 14:47:56 +0100

    homePage--> homePage WIP

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -8,22 +8,31 @@ function HomePage() {
     <div className="page">
       <div className="home-content">
         <div className="image-container" style={{ backgroundImage: `url(${homeBackground})` }}>
+          <div class="text">
+            FIND YOUR HOME TO LIVE
+          </div>
           <div className="logo-container">
             <img src={logo} alt="Logo" className="logo" />
           </div>
         </div>
       </div>
       <div className="button-container">
-  <a href="https://www.example.com" target="_blank" rel="noopener noreferrer">
-    List your property
-  </a>
-  <a href="https://www.example.com" target="_blank" rel="noopener noreferrer">
-    Find more about Lisbon
-  </a>
-  <a href="https://www.example.com" target="_blank" rel="noopener noreferrer">
-    Offers and promotions
-  </a>
-</div>
+        <div className="list-property-button">
+          <a href="https://www.example.com" target="_blank" rel="noopener noreferrer">
+            List your property
+          </a>
+        </div>
+        <div className="more-about-lisbon-button">
+          <a href="https://www.example.com" target="_blank" rel="noopener noreferrer">
+            Find more about Lisbon
+          </a>
+        </div>
+        <div className="offers-promotions-button">
+          <a href="https://www.example.com" target="_blank" rel="noopener noreferrer">
+            Offers and promotions
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
