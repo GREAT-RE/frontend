7173e2f21083b7fdd831f42a760837fc8ee44a0b
JoseSNunes <josesimasnunes@gmail.com>
2023-04-26 14:32:00 +0100

    Some changes on router/routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import HomePage from "./Pages/HomePage/HomePage";
 import './App.css';
 import PropertyList from "./Pages/PropertyList/PropertyList.jsx"
@@ -7,12 +7,10 @@ import PropertyList from "./Pages/PropertyList/PropertyList.jsx"
 
 function App() {
   return (
-    <Router>
       <Routes>
         <Route exact path="/property-list" element={<PropertyList />} />
         <Route exact path="/" element={<HomePage />} />
       </Routes>
-    </Router>
   );
 }
 
