4324ecec9eb89e11009a04e26379d631db89db38
PedroGLL27 <pedrolou27@gmail.com>
2023-04-26 12:33:02 +0100

    prpoertypage WIP

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HomePage from './pages/HomePage/HomePage';
+import HomePage from './pages/HomePage/HomePage.jsx';
 import './App.css';
 import PropertyList from "./Pages/PropertyList/PropertyList.jsx"
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 
 function App() {
   return (
