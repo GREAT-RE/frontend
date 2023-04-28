a70f2282193c63b7088846e8c8586f27b7dcd44c
PedroGLL27 <pedrolou27@gmail.com>
2023-04-28 18:17:31 +0100

    some changes

--- src/Pages/Offers&Promo/Offers&Promo.jsx
+++ src/Pages/Offers&Promo/Offers&Promo.jsx
@@ -1,18 +1,25 @@
 import React from "react"; 
-import offerIMG from "../../assets/off_promo.jpg"
-import './Offers&Promo.css'
+import offerIMG from "../../assets/off_promo.jpg";
+import './Offers&Promo.css';
 
 function OfferPromo() {
   return (
     <div className="offer-promo">
-        <div className="image-container" style={{ backgroundImage: `url(${offerIMG})` }}>
-          <div className="text">
-            GET TO KNOW OUR PARTNERS
-          </div>
+      <div className="offer-image" style={{ backgroundImage: `url(${offerIMG})` }}>
+        <div className="offer-text">
+          GET TO KNOW OUR PARTNERS
         </div>
+      </div>
+      <div className="partners-container">
+        <i className="fa fa-icon-1"></i>
+        <i className="fa fa-icon-2"></i>
+        <i className="fa fa-icon-3"></i>
+        <i className="fa fa-icon-4"></i>
+      </div>
     </div>
   );
 }
 
 export default OfferPromo;
+
 
