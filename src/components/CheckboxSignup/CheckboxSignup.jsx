1298af60588e665859434a06218767fcdc9f18a7
PedroGLL27 <pedrolou27@gmail.com>
2023-05-10 11:22:27 +0100

    some changes on checkboxsignup

--- src/components/CheckboxSignup/CheckboxSignup.jsx
+++ src/components/CheckboxSignup/CheckboxSignup.jsx
@@ -5,15 +5,17 @@ const CheckboxSignup = () => {
   return (
     <div className="checkbox-signup-container">
 
-      <form>
-        <label className="checkbox-label" >
-          <input className="terms" type="checkbox" name="terms" value="true" />
-          By creating an account you are agreeing to accept the <br></br> 
-          <a href="#">Terms and conditions</a> and <a href="#">Privacy policy</a>
-        </label>
-
+  <form>
+  <label className="checkbox-label">
+    <input className="terms" type="checkbox" name="terms" value="true" />
+    <span className="checkbox-text">
+      By creating an account you are agreeing to accept the<br />
+      <a href="#" className="terms-link">Terms and Conditions</a> and<br />
+      <a href="#" className="privacy-link">Privacy Policy</a>
+    </span>
+  </label>
         <label className="checkbox-label">
-          <input  classname=""type="checkbox" name="newsletter" value="true" />
+          <input  className=""type="checkbox" name="newsletter" value="true" />
           I'd like to receive e-mails and promotions from Home in LX.
         </label>
         
