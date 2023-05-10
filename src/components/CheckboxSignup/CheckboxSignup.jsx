fee27478753ccfd682a0eac64cd87fbd030d5388
PedroGLL27 <pedrolou27@gmail.com>
2023-05-10 17:51:17 +0100

    checkbox resolved

--- src/components/CheckboxSignup/CheckboxSignup.jsx
+++ src/components/CheckboxSignup/CheckboxSignup.jsx
@@ -4,20 +4,7 @@ import './CheckboxSignup.css'
 const CheckboxSignup = () => {
   return (
     <div className="checkbox-signup-container">
-
-
       <form>
-        <label className="checkbox-label" >
-          <input className="terms" type="checkbox" name="terms" value="true" />
-          By creating an account you are agreeing to accept the 
-          <br />
-          <a href="#">Terms and conditions</a> and <a href="#">Privacy policy</a>
-        </label>
-
-        <label className="checkbox-label">
-          <input  classname=""type="checkbox" name="newsletter" value="true" />
-
-  <form>
   <label className="checkbox-label">
     <input className="terms" type="checkbox" name="terms" value="true" />
     <span className="checkbox-text">
@@ -37,9 +24,8 @@ const CheckboxSignup = () => {
         <p>Already have an account? <a href="https://example.com" className="signASK">Sign up</a></p>
 
       </form>
-
     </div>
   );
 };
 
-export default CheckboxSignup;
+export default CheckboxSignup
